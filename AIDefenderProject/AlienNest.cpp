5481d277899bfb7ac35196ffd2fde3b25b8a3d44
OloinMilsom <keporagebora@gmail.com>
2017-01-05 14:46:46 +0000

    amended minor issue, test statement still in code

--- AIDefenderProject/AlienNest.cpp
+++ AIDefenderProject/AlienNest.cpp
@@ -21,7 +21,6 @@ void AlienNest::update(float dt, AlienManager * data) {
 	m_spawnTimer -= dt;
 	if (m_spawnTimer < 0) {
 		data->addAbductor(m_position);
-		data->addMutant(m_position);
 		resetSpawnTimer();
 	}
 
