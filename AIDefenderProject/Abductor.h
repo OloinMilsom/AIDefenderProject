24f86cc9d3b4a4fa485e37a9d272653ed8558bc1
OloinMilsom <keporagebora@gmail.com>
2017-01-08 14:06:20 +0000

    added flocking behaviour, constants need tweaking

--- AIDefenderProject/Abductor.h
+++ AIDefenderProject/Abductor.h
@@ -4,7 +4,13 @@
 
 class Abductor : public Alien {
 private:
+	const float FLOCK_DISTANCE = 100;
+	const float ALIGNMENT = 100;
+	const float COHESION = 1000;
+	const float SEPARATION = 150;
 
+	void flock(AlienManager * data);
+	void combineAcceleration(sf::Vector2f other);
 public:
 	Abductor(sf::Vector2f position, float speed, float acceleration);
 	void update(float dt, AlienManager * data);
--- AIDefenderProject/Alien.cpp
+++ AIDefenderProject/Alien.cpp
@@ -20,8 +20,64 @@ void Alien::render(sf::RenderWindow * window, Camera * camera) {
 	window->draw(velAccel);
 }
 
+void Alien::move(float dt) {
+	m_acceleration *= MAX_ACCELERATION / sqrt(m_acceleration.x * m_acceleration.x + m_acceleration.y * m_acceleration.y);
+
+	m_velocity += m_acceleration * dt;
+
+	float velLengthSquared = m_velocity.x * m_velocity.x + m_velocity.y * m_velocity.y;
+	if (velLengthSquared > MAX_SPEED * MAX_SPEED) {
+		m_velocity *= MAX_SPEED / sqrt(velLengthSquared);
+	}
+
+	m_velocity -= m_velocity * 0.1f * dt;
+
+	m_position += m_velocity * dt;
+
+	m_sprite.setPosition(m_position);
+}
+
+void Alien::wander() {
+	if (rand() % 60 == 0) {
+		float angle = 0.5f;
+		if (rand() % 2 == 0) {
+			angle = -angle;
+		}
+		m_acceleration = sf::Vector2f(cos(angle)*m_acceleration.x - sin(angle)*m_acceleration.y, sin(angle)*m_acceleration.x + cos(angle)*m_acceleration.y);
+	}
+}
+
+void Alien::avoidBounds(const Terrain * terrain) {
+	if (m_velocity.y < 0) {
+		m_acceleration.y += MAX_ACCELERATION * 500 / (m_position.y * m_position.y);
+	}
+	if (m_position.y < 0) {
+		m_position.y = 0;
+		m_velocity.y = 0;
+	}
+
+	float pointAtGround = terrain->getHeightAt(m_position.x);
+	if (m_velocity.y > 0) {
+		m_acceleration.y -= MAX_ACCELERATION * 500 / ((pointAtGround - m_position.y) * (pointAtGround - m_position.y));
+	}
+	if (m_position.y > pointAtGround) {
+		m_position.y = pointAtGround;
+		m_velocity.y = 0;
+	}
+}
+
 AlienType Alien::getType() {
 	return m_type;
 }
 
-sf::Vector2f Alien::getPos(){ return m_position;}
+sf::Vector2f Alien::getPos() { 
+	return m_position;
+}
+
+sf::Vector2f Alien::getVel() {
+	return m_velocity;
+}
+
+sf::Vector2f Alien::getAccel() {
+	return m_acceleration;
+}
